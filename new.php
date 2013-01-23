cb3f3e648bd5755f5b4ba90924d3ca55c921c65c
Frank Hebbert <frank@hebbert.com>
2013-01-23 00:34:35 -0500

    swapping in pg escape

--- new.php
+++ new.php
@@ -5,9 +5,8 @@ include('functions.php');
 $nabe = $_POST['neighborhood'];
 $block = $_POST['block'];
 
-$clean_nabe = mysql_real_escape_string($nabe);
-$clean_block = mysql_real_escape_string($block);
-
+$clean_nabe = pg_escape_string($nabe);
+$clean_block = pg_escape_string($block);
 
 addNeighborhoodName($clean_nabe, $clean_block);
 
